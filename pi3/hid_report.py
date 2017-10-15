498dea668f0c78b641ff8436b5e26b6aa516d07c
Juuso Tuononen <jutuon@outlook.com>
2017-10-15 14:58:28 +0300

    HidReport class updates

--- pi3/hid_report.py
+++ pi3/hid_report.py
@@ -2,6 +2,35 @@
 
 import evdev
 
+EVDEV_TO_HID_MAP = {
+    evdev.ecodes.KEY_A: 0x04,
+    evdev.ecodes.KEY_B: 0x05,
+    evdev.ecodes.KEY_C: 0x06,
+    evdev.ecodes.KEY_D: 0x07,
+    evdev.ecodes.KEY_E: 0x08,
+    evdev.ecodes.KEY_F: 0x09,
+    evdev.ecodes.KEY_G: 0x0A,
+    evdev.ecodes.KEY_H: 0x0B,
+    evdev.ecodes.KEY_I: 0x0C,
+    evdev.ecodes.KEY_J: 0x0D,
+    evdev.ecodes.KEY_K: 0x0E,
+    evdev.ecodes.KEY_L: 0x0F,
+    evdev.ecodes.KEY_M: 0x10,
+    evdev.ecodes.KEY_N: 0x11,
+    evdev.ecodes.KEY_O: 0x12,
+    evdev.ecodes.KEY_P: 0x13,
+    evdev.ecodes.KEY_Q: 0x14,
+    evdev.ecodes.KEY_R: 0x15,
+    evdev.ecodes.KEY_S: 0x16,
+    evdev.ecodes.KEY_T: 0x17,
+    evdev.ecodes.KEY_U: 0x18,
+    evdev.ecodes.KEY_V: 0x19,
+    evdev.ecodes.KEY_W: 0x1A,
+    evdev.ecodes.KEY_X: 0x1B,
+    evdev.ecodes.KEY_Y: 0x1C,
+    evdev.ecodes.KEY_Z: 0x1D,
+}
+
 
 class HidReport:
 
@@ -18,8 +47,45 @@ class HidReport:
         self.right_alt = False
         self.right_gui = False
 
-        self.keycodes = bytearray(6)
+        self.keycodes = {}
 
+    def modifier_keys(self) -> bytes:
+        bit_flag = 0x00
+
+        if self.left_control:
+            bit_flag = bit_flag | 0b10000000
+        if self.left_shift:
+            bit_flag = bit_flag | 0b01000000
+        if self.left_alt:
+            bit_flag = bit_flag | 0b00100000
+        if self.left_gui:
+            bit_flag = bit_flag | 0b00010000
+        if self.right_control:
+            bit_flag = bit_flag | 0b00001000
+        if self.right_shift:
+            bit_flag = bit_flag | 0b00000100
+        if self.right_alt:
+            bit_flag = bit_flag | 0b00000010
+        if self.right_gui:
+            bit_flag = bit_flag | 0b00000001
+
+        return bytes([bit_flag])
+
+    def add_key(self, evdev_key: int):
+        if len(self.keycodes) >= 6:
+            return
+
+        self.keycodes[evdev_key] = EVDEV_TO_HID_MAP[evdev_key]
+
+    def remove_key(self, evdev_key: int):
+        del self.keycodes[evdev_key]
+
+    def send(self):
+        byte_1 = self.modifier_keys()
+        print("modifier_keys: {0:#b}".format(byte_1[0]))
+
+        for key in self.keycodes:
+            print("keycode: {0:#x}".format(self.keycodes[key]))
 
 
 
--- pi3/main.py
+++ pi3/main.py
@@ -1,10 +1,13 @@
 #!/bin/python3
 
 import evdev
+import hid_report
 
 def main():
-    print(evdev.ecodes.KEY_A)
-    print("test")
+    x = hid_report.HidReport()
+    x.left_control = True
+    x.add_key(evdev.ecodes.KEY_A)
+    x.send()
 
 
 if __name__ == "__main__":

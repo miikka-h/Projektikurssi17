ec0d07299eee05dd8788cc39428ce14349c2c78a
Juuso Tuononen <jutuon@outlook.com>
2017-10-27 19:29:12 +0300

    zero-client update

--- zero-client/zero-client.py
+++ zero-client/zero-client.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 
 import os
+import sys
+import socket
 
 # http://www.usb.org/developers/hidpage/Hut1_12v2.pdf
 # See chapter 10 for keycodes.
@@ -15,14 +17,15 @@ import os
 # byte 3-8: keycode values from 0-101
 
 
-# TODO: create TCP socket
+# Check arguments
 
-# key 'a'
-HID_REPORT = [0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00]
-HID_REPORT_BYTES = bytes(HID_REPORT)
+if len(sys.argv) <= 2:
+    print("give server address and server port number as arguments")
+    exit(-1)
 
-EMPTY_HID_REPORT = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-EMPTY_HID_REPORT_BYTES = bytes(EMPTY_HID_REPORT)
+
+
+# Open file.
 
 FILE_NAME = "/dev/hidg0"
 
@@ -30,9 +33,43 @@ if not os.path.exists(FILE_NAME):
     print(FILE_NAME + " does not exist")
     exit(-1)
 
-with open(FILE_NAME, "ab") as file:
-    file.write(HID_REPORT_BYTES)
-    file.write(EMPTY_HID_REPORT_BYTES)
+file = open(FILE_NAME, "ab")
+
+# Connect to server
+
+HID_REPORT_SIZE_BYTES = 8
+
+print("trying to connect to the server")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((sys.argv[1], int(sys.argv[2])))
+
+print("connected to to server")
+
+try:
+    while True:
+        data = s.recv(HID_REPORT_SIZE_BYTES)
+        byte_count = len(data)
+
+        if byte_count < HID_REPORT_SIZE_BYTES:
+            print("received " + str(byte_count) + " bytes")
+            print("error: received less than " + str(HID_REPORT_SIZE_BYTES) + " bytes")
+            s.close()
+            file.close()
+            exit(-1)
+
+        file.write(data)
+except OSError as error:
+    print("error: " + error.strerror)
+    file.close()
+    s.close()
+except KeyboardInterrupt:
+    file.close()
+    s.close()
 
 
-print("appending to " + FILE_NAME + " ok")
+
+
+
+

b0ed85cef482e1f8575a0291fe466114b3625b2b
Juuso Tuononen <jutuon@outlook.com>
2017-10-27 19:55:53 +0300

    remove old main.py and some other files

--- pi3/main.py
+++ pi3/main.py
@@ -15,106 +15,92 @@ from hid_report import HidReport
 
 def main():
 
-    # Check argument count.
-
-    if len(sys.argv) < 2:
-        print("give evdev device file path as argument")
-        exit(-1)
-
-    # Start web server.
-
-    exit_event = Event()
-
-    web_server_settings = ("", 8080)
-    settings_queue = Queue()
-
-    web_server_thread = Thread(group=None, target=WebServer, args=(web_server_settings, settings_queue, exit_event))
-    web_server_thread.start()
-
-    # Initialize input event reading.
-
-    input_device = evdev.InputDevice(sys.argv[1])
-    print(input_device)
-
-    # Create evdev keycode to USB HID report converter.
-
-    hid_report = HidReport()
-
-    # Socket where USB HID data will be sent.
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind(("", 25001))
-        server_socket.listen(0)
-    except OSError as error:
-        print("error: " + error.strerror)
-        exit_event.set()
-        web_server_thread.join()
-        server_socket.close()
-        input_device.close()
-        exit(-1)
 
-    # Wait client to connect.
+        if len(sys.argv) < 2:
+            print("give evdev device file path as argument")
+            exit(-1)
 
-    print("waiting for client")
+        # Firts we load all necessary components and form connections.
 
-    connection_socket = None
-    address = None
+        server = Server()
 
-    try:
-        (connection_socket, address) = server_socket.accept()
+        # hid data will be sent here. defined in the try block.
+        socket_out = None
+
+        try:
+            socket_out = Socket_out()
+        except OSError as error:
+            print("error: " + error.strerror)
+            server.close()
+            socket_out.close()
+            exit(-1)
+
+        print("waiting for client")
+
+        socket_out.findConnection()
+
+        print("client from " + str(socket_out.address) + " connected")
+
+        # Create evdev keycode to USB HID report converter.
+        hid_report = HidReport()
+
+        # Initialize input event reading.
+        input_device = evdev.InputDevice(sys.argv[1])
+        print(input_device)
+
+        # Actual server logic loop.
+        run(server, socket_out, hid_report, input_device)
+
     except KeyboardInterrupt:
         # handle ctrl-c
-        exit_event.set()
-        web_server_thread.join()
-        server_socket.close()
+        server.close()
+        socket_out.close()
+        hid_report.close()
         input_device.close()
         exit(0)
 
-    print("client from " + str(address) + " connected")
 
-    # Server main loop.
+def run(server, socket_out, hid_report, input_device):
 
     clear_keys = True
 
-    try:
+    while True:
+        try:
+            new_settings = server.settings_queue.get(block=False)
+            print(str(new_settings))
+        except Empty:
+            pass
+
+        key_update = False
+
         while True:
-            try:
-                new_settings = settings_queue.get(block=False)
-                print(str(new_settings))
-            except Empty:
-                pass
+            event = input_device.read_one()
 
-            key_update = False
+            if event is None:
+                clear_keys = False
+                break
 
-            while True:
-                event = input_device.read_one()
+            # add keys that are currently pressed down to the hid report.
+            if event.type == ecodes.EV_KEY and not clear_keys:
+                key_event = evdev.categorize(event)
 
-                if event is None:
-                    clear_keys = False
-                    break
-
-                if event.type == ecodes.EV_KEY and not clear_keys:
-                    key_event = evdev.categorize(event)
-
-                    if key_event.keystate == key_event.key_down:
-                        hid_report.add_key(key_event.scancode)
-                        key_update = True
-                    elif key_event.keystate == key_event.key_up:
-                        hid_report.remove_key(key_event.scancode)
-                        key_update = True
+                if key_event.keystate == key_event.key_down:
+                    hid_report.add_key(key_event.scancode)
+                    key_update = True
+                elif key_event.keystate == key_event.key_up:
+                    hid_report.remove_key(key_event.scancode)
+                    key_update = True
 
             if key_update:
                 hid_report.update_report()
 
                 try:
-                    connection_socket.sendall(hid_report.report)
+                    socket_out.connection_socket.sendall(hid_report.report)
                 except OSError as error:
                     print("error: " + error.strerror)
-                    print("disconnecting client from: " + str(address))
-                    connection_socket.close()
+                    print("disconnecting client from: " + str(server.address))
+                    socket_out.connection_socket.close()
                     print("waiting for new client")
                     (connection_socket, address) = server_socket.accept()
                     print("client from " + str(address) + " connected")
@@ -122,13 +108,37 @@ def main():
 
             time.sleep(0.01)
 
-    except KeyboardInterrupt:
-        # handle ctrl-c
-        exit_event.set()
-        web_server_thread.join()
-        connection_socket.close()
-        server_socket.close()
-        input_device.close()
+
+class Socket_out():
+
+    def __init__(self):
+        self.connection_socket = None
+        self.address = None
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind(("", 25001))
+        self.server_socket.listen(0)
+
+    def close(self):
+        self.server_socket.close()
+
+    def findConnection(self):
+        (self.connection_socket, self.address) = self.server_socket.accept()
+
+
+class Server():
+
+    def __init__(self):
+        self.exit_event = Event()
+        web_server_settings = ("", 8080)
+        self.settings_queue = Queue()
+        self.web_server_thread =Thread(group=None, target=WebServer, args=(web_server_settings, self.settings_queue, self.exit_event))
+        self.web_server_thread.start()
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def close(self):
+        self.exit_event.set()
+        self.web_server_thread.join()
 
 
 if __name__ == "__main__":

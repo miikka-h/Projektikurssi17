b907931629960a36998c682e21223758a24bea90
Miikka Heiskanen <miikka.heiskanen@yahoo.com>
2017-10-28 16:00:04 +0300

    Added key ID:s to hid_report, started working on get- and post methods on web_server, new file "keyprofile" which has information of key_id:s and mapped key_id:s etc.

--- pi3/hid_report.py
+++ pi3/hid_report.py
@@ -1,5 +1,3 @@
-
-
 import evdev
 
 EVDEV_TO_HID_MAP = {
@@ -29,6 +27,66 @@ EVDEV_TO_HID_MAP = {
     evdev.ecodes.KEY_X: 0x1B,
     evdev.ecodes.KEY_Y: 0x1C,
     evdev.ecodes.KEY_Z: 0x1D,
+    evdev.ecodes.KEY_1: 0x1E,
+    evdev.ecodes.KEY_2: 0x1F,
+    evdev.ecodes.KEY_3: 0x20,
+    evdev.ecodes.KEY_4: 0x21,
+    evdev.ecodes.KEY_5: 0x22,
+    evdev.ecodes.KEY_6: 0x23,
+    evdev.ecodes.KEY_7: 0x24,
+    evdev.ecodes.KEY_8: 0x25,
+    evdev.ecodes.KEY_9: 0x26,
+    evdev.ecodes.KEY_0: 0x27,
+    evdev.ecodes.KEY_ENTER: 0x28,
+    evdev.ecodes.KEY_ESC: 0x29,
+    evdev.ecodes.KEY_BACKSPACE: 0x2A,
+    evdev.ecodes.KEY_TAB: 0x2B,
+    evdev.ecodes.KEY_SPACE: 0x2C,
+    evdev.ecodes.KEY_MINUS: 0x2D,
+    evdev.ecodes.KEY_F1: 0x3A,
+    evdev.ecodes.KEY_F2: 0x3B,
+    evdev.ecodes.KEY_F3: 0x3C,
+    evdev.ecodes.KEY_F4: 0x3D,
+    evdev.ecodes.KEY_F5: 0x3E,
+    evdev.ecodes.KEY_F6: 0x3F,
+    evdev.ecodes.KEY_F7: 0x40,
+    evdev.ecodes.KEY_F8: 0x41,
+    evdev.ecodes.KEY_F9: 0x42,
+    evdev.ecodes.KEY_F10: 0x43,
+    evdev.ecodes.KEY_F11: 0x44,
+    evdev.ecodes.KEY_F12: 0x45,
+    evdev.ecodes.KEY_PRINT: 0x46,
+    evdev.ecodes.KEY_SCROLLLOCK: 0x47,
+    evdev.ecodes.KEY_PAUSE: 0x48,
+    evdev.ecodes.KEY_INSERT: 0x49,
+    evdev.ecodes.KEY_HOME: 0x4A,
+    evdev.ecodes.KEY_PAGEUP: 0x4B,
+    evdev.ecodes.KEY_DELETE: 0x4C,
+    evdev.ecodes.KEY_END: 0x4D,
+    evdev.ecodes.KEY_PAGEDOWN: 0x4E,
+    evdev.ecodes.KEY_RIGHT: 0x4F,
+    evdev.ecodes.KEY_LEFT: 0x50,
+    evdev.ecodes.KEY_DOWN: 0x51,
+    evdev.ecodes.KEY_UP: 0x52,
+    evdev.ecodes.KEY_NUMLOCK: 0x53,
+    evdev.ecodes.KEY_KPSLASH: 0x54,
+    evdev.ecodes.KEY_KPASTERISK: 0x55,
+    evdev.ecodes.KEY_KPMINUS: 0x56,
+    evdev.ecodes.KEY_KPPLUS: 0x57,
+    evdev.ecodes.KEY_KPENTER: 0x58,
+    evdev.ecodes.KEY_KP1: 0x59,
+    evdev.ecodes.KEY_KP2: 0x5A,
+    evdev.ecodes.KEY_KP3: 0x5B,
+    evdev.ecodes.KEY_KP4: 0x5C,
+    evdev.ecodes.KEY_KP5: 0x5D,
+    evdev.ecodes.KEY_KP6: 0x5E,
+    evdev.ecodes.KEY_KP7: 0x5F,
+    evdev.ecodes.KEY_KP8: 0x60,
+    evdev.ecodes.KEY_KP9: 0x61,
+    evdev.ecodes.KEY_KP0: 0x62,
+    evdev.ecodes.KEY_APOSTROPHE: 0x63,
+	#TODO find out how to refer to the rest of the keys	
+
 }
 
 
@@ -99,7 +157,3 @@ class HidReport:
 
         for j in range(i, 8):
             self.report[j] = 0x00
-
-
-
-

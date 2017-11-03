4eb4d202d1d878301f3d67fcdddf0119a4a2283b
Juuso Tuononen <jutuon@outlook.com>
2017-11-03 18:42:55 +0200

    zero-client: error handling updates

--- zero-client/zero-client.py
+++ zero-client/zero-client.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import socket
+import time
 
 # http://www.usb.org/developers/hidpage/Hut1_12v2.pdf
 # See chapter 10 for keycodes.
@@ -16,61 +17,77 @@ import socket
 
 # byte 3-8: keycode values from 0-101
 
-
-# Check arguments
-
-if len(sys.argv) <= 2:
-    print("give server address and server port number as arguments")
-    exit(-1)
-
-
-
-# Open file.
-
 FILE_NAME = "/dev/hidg0"
-
-if not os.path.exists(FILE_NAME):
-    print(FILE_NAME + " does not exist")
-    exit(-1)
-
-file = open(FILE_NAME, "ab")
-
-# Connect to server
-
 HID_REPORT_SIZE_BYTES = 8
 
-print("trying to connect to the server")
+# Functions
+def try_to_connect(socket_object: socket.SocketType, server_address: str, port_number: int) -> bool:
+    try:
+        socket_object.connect((server_address, port_number))
+    except OSError as error:
+        print("error: " + error.strerror)
+        return False
+    return True
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect((sys.argv[1], int(sys.argv[2])))
-
-print("connected to to server")
-
-try:
+def connection_retry_loop(socket_object: socket.SocketType, server_address: str, port_number: int) -> None:
     while True:
-        data = s.recv(HID_REPORT_SIZE_BYTES)
-        byte_count = len(data)
+        if try_to_connect(socket_object, server_address, port_number):
+            print("connected to the server")
+            break
+        else:
+            print("trying to reconnect to the server in 5 seconds")
+            time.sleep(5.0)
 
-        if byte_count < HID_REPORT_SIZE_BYTES:
-            print("received " + str(byte_count) + " bytes")
-            print("error: received less than " + str(HID_REPORT_SIZE_BYTES) + " bytes")
-            s.close()
-            file.close()
-            exit(-1)
+def main() -> None:
+    # Check arguments
 
-        file.write(data)
-        file.flush()
-except OSError as error:
-    print("error: " + error.strerror)
-    file.close()
-    s.close()
-except KeyboardInterrupt:
-    file.close()
-    s.close()
+    if len(sys.argv) <= 2:
+        print("give server address and server port number as arguments")
+        exit(-1)
 
+    # Open file.
 
+    if not os.path.exists(FILE_NAME):
+        print(FILE_NAME + " does not exist")
+        exit(-1)
 
+    file = open(FILE_NAME, "ab")
 
+    # Create socket.
 
+    socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    server_address = sys.argv[1]
+    port_number = int(sys.argv[2])
+
+    try:
+        print("trying to connect to the server")
+        connection_retry_loop(socket_object, server_address, port_number)
+
+        while True:
+            data = socket_object.recv(HID_REPORT_SIZE_BYTES)
+            byte_count = len(data)
+
+            if byte_count == 0:
+                print("server disconnected")
+                print("trying to reconnect")
+                socket_object.close()
+                socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                connection_retry_loop(socket_object, server_address, port_number)
+            elif byte_count != HID_REPORT_SIZE_BYTES:
+                print("error: USB HID report size " + str(byte_count) + " bytes is unsupported")
+                socket_object.close()
+                file.close()
+                exit(-1)
+            else:
+                # No errors
+                file.write(data)
+                file.flush()
+
+    except OSError as error:
+        print("error: " + error.strerror)
+        file.close()
+        socket_object.close()
+    except KeyboardInterrupt:
+        file.close()
+        socket_object.close()
